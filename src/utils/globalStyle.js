768588571099daaeea16a53c4b5a79c3f538ea66
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2020-06-19 01:47:02 +0300

    style added into globalStyle.js component.

--- src/utils/globalStyle.js
+++ src/utils/globalStyle.js
@@ -3,7 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   html {
     font-size: 62.5%;
+
   }
+
+  a,
+    a:hover,
+    a.active,
+    a:focus {
+        text-decoration: none;
+    }
 `;
 
 export default GlobalStyle;
